3cb8ece82007624b8e82e13321af136ba0853f2a
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-01 16:18:39 +0200

    selenium framework

---
+++ locatorBy.ID.py
@@ -0,0 +1,23 @@
+
+# importing webdriver from selenium
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+# Here FireFox  will be used
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+# This is for maximize web window
+driver.maximize_window()
+
+# URL of website
+url = "https://popageorgianvictor.github.io/MyTestSite/"
+
+# Opening the website
+driver.get(url)
+
+# The form element can be located like this
+login_form = driver.find_element(By.ID, 'loginForm')
+print(login_form)
