f9912fa62a43005c22fc2c91ec4d3c6f4010f05b
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-01 17:57:32 +0200

    selenium framework

---
+++ locatorsBy.TAG_NAME.py
@@ -0,0 +1,22 @@
+# importing webdriver from selenium
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+# Here FireFox  will be used
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+# This is for maximize web window
+driver.maximize_window()
+
+# URL of website
+url = "https://popageorgianvictor.github.io/MyTestSite/"
+
+# Opening the website
+driver.get(url)
+
+# The heading (h1) element can be located like this
+heading1 = driver.find_element(By.TAG_NAME, 'h1')
+print(heading1)
