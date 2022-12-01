b2bdddd25319c1aced5880843007862f5441d7f1
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-01 16:31:06 +0200

    selenium framework

---
+++ locatorBy.NAME.py
@@ -0,0 +1,25 @@
+
+# importing webdriver from selenium
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+# Here FireFox  will be used
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+# This is for maximize web window
+driver.maximize_window()
+
+# URL of website
+url = "https://popageorgianvictor.github.io/MyTestSite/"
+
+# Opening the website
+driver.get(url)
+
+# The username & password elements can be located like this:
+username = driver.find_element(By.NAME, 'username')
+password = driver.find_element(By.NAME, 'password')
+print(username)
+print(password)
