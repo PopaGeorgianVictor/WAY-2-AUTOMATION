638d7e878327c5e8e4c19540324611b89e50005f
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-02 19:02:43 +0200

    selenium framework

---
+++ explicit_waits.py
@@ -0,0 +1,33 @@
+
+# import necessary classes
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# create driver object
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+# URL of website
+url = "https://popageorgianvictor.github.io/MyTestSite/"
+
+# Opening the website
+driver.get(url)
+
+# Maximize web window
+driver.maximize_window()
+
+
+try:
+    # wait 10 second before looking for element
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'the_slow_image'))
+    )
+
+finally:
+    # else quit
+    driver.quit()
+
