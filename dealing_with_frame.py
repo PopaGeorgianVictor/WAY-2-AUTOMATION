00f5a6f65cdcc672e66565ea53449b91ab731a62
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-06 11:08:36 +0200

    selenium framework

--- dealing_with_frame.py
+++ dealing_with_frame.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
+
 
 
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
