ca7168c1edb8af29587d20a9e3810c983e7687c2
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-11 19:24:42 +0200

    way 2 automation

--- alert_confirm_prompt.py
+++ alert_confirm_prompt.py
@@ -13,37 +13,45 @@ driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/alert_confir
 driver.maximize_window()
 driver.implicitly_wait(2)
 
-# # html alert
-# driver.find_element(By.CSS_SELECTOR, "div#bootStrapAlertExample button").click()
-# html_alert_text = driver.find_element(By.ID, "bootStrapAlert").text
-# expected_text = "This is alert using just html."
-# assert html_alert_text == expected_text, f"Error: expected: {expected_text}, actual: {html_alert_text}"
-#
-# # js alert accept
-# driver.find_element(By.CSS_SELECTOR, "div#jsAlertExample button").click()
-# js_alert = driver.switch_to.alert
-# js_alert.accept()
+# html alert
+driver.find_element(By.CSS_SELECTOR, "div#bootStrapAlertExample button").click()
+html_alert_text = driver.find_element(By.ID, "bootStrapAlert").text
+expected_text = "This is alert using just html."
+assert html_alert_text == expected_text, f"Error: expected: {expected_text}, actual: {html_alert_text}"
 
-# # js confirm accept alert
-# driver.find_element(By.CSS_SELECTOR, "div#jsConfirmExample button").click()
-# js_confirm = driver.switch_to.alert
-# js_confirm.accept()
-# rs_message = driver.find_element(By.ID, 'userResponse1').text
-# assert rs_message == 'Great! You will love it!', "Wrong message after accepting"
+# js alert accept
+driver.find_element(By.CSS_SELECTOR, "div#jsAlertExample button").click()
+js_alert = driver.switch_to.alert
+js_alert.accept()
 
-# # js confirm cancel alert
-# driver.find_element(By.CSS_SELECTOR, "div#jsConfirmExample button").click()
-# js_confirm = driver.switch_to.alert
-# js_confirm.dismiss()
-# rs_message = driver.find_element(By.ID, 'userResponse1').text
-# assert rs_message == "Too bad!!! You would've loved it!", "Wrong message after accepting"
+# js confirm accept alert
+driver.find_element(By.CSS_SELECTOR, "div#jsConfirmExample button").click()
+js_confirm = driver.switch_to.alert
+js_confirm.accept()
+rs_message = driver.find_element(By.ID, 'userResponse1').text
+assert rs_message == 'Great! You will love it!', "Wrong message after accepting"
+
+# js confirm cancel alert
+driver.find_element(By.CSS_SELECTOR, "div#jsConfirmExample button").click()
+js_confirm = driver.switch_to.alert
+js_confirm.dismiss()
+rs_message = driver.find_element(By.ID, 'userResponse1').text
+assert rs_message == "Too bad!!! You would've loved it!", "Wrong message after accepting"
 
 # js prompt accept alert with text
-# driver.find_element(By.CSS_SELECTOR, "div#jsPromptExample button").click()
-# js_prompt = driver.switch_to.alert
-# inserted_text = 'test'
-# js_prompt.send_keys(inserted_text)
-# js_prompt.accept()
-# rs_message = driver.find_element(By.ID, 'userResponse2').text
-# expected_text = f"You have entered: {inserted_text}"
-# assert rs_message == expected_text, f"Error: expected: {expected_text}, actual: {rs_message}"
+driver.find_element(By.CSS_SELECTOR, "div#jsPromptExample button").click()
+js_prompt = driver.switch_to.alert
+inserted_text = 'test'
+js_prompt.send_keys(inserted_text)
+js_prompt.accept()
+rs_message = driver.find_element(By.ID, 'userResponse2').text
+expected_text = f"You have entered: {inserted_text}"
+assert rs_message == expected_text, f"Error: expected: {expected_text}, actual: {rs_message}"
+
+# js prompt accept alert without text
+driver.find_element(By.CSS_SELECTOR, "div#jsPromptExample button").click()
+js_prompt = driver.switch_to.alert
+js_prompt.accept()
+rs_message = driver.find_element(By.ID, 'userResponse2').text
+expected_text = f"You have entered: none"
+assert rs_message == expected_text, f"Error: expected: {expected_text}, actual: {rs_message}"
