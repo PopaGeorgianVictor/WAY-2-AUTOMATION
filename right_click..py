934296eebeaeb08afd57811bd203651d7aecb3fe
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-15 19:41:36 +0200

    way 2 automation

--- right_click..py
+++ right_click..py
@@ -8,13 +8,13 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 driver.maximize_window()
-driver.implicitly_wait(1)
+driver.implicitly_wait(2)
 
-driver.get("https://popageorgianvictor.github.io/My-Test-Site/")
+driver.get("https://popageorgianvictor.github.io/PRESENTATION-SITE/")
 
 
 # Right click action
 
-right_click_menu = driver.find_element(By.XPATH, "/html/body/div/nav/ul/li[4]/a")
+right_click_menu = driver.find_element(By.CSS_SELECTOR, "div#container a")
 ActionChains(driver).context_click(right_click_menu).perform()# context click = right click
 
