6e5acddcd7a7a52fd1958417da5de487f0e55fbb
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-15 15:10:19 +0200

    way 2 automation

--- alert_confirm_prompt.py
+++ alert_confirm_prompt.py
@@ -1,8 +1,5 @@
-import time
+
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
