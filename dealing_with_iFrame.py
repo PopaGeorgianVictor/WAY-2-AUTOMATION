87c323871b9c6b8d525afa4373c09505155c0b11
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-15 15:29:56 +0200

    way 2 automation

--- dealing_with_iFrame.py
+++ dealing_with_iFrame.py
@@ -1,4 +1,4 @@
-import time
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
@@ -10,17 +10,14 @@ driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 # maximize web window
 driver.maximize_window()
-driver.implicitly_wait(1)
+
 
 # URL of website
-url = "https://popageorgianvictor.github.io/MyTestSite/"
+url = "https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/iFrame"
 
 # Opening the website
 driver.get(url)
 
-# Scroll down in page
-driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.ID, "iFrames"))
-time.sleep(2)
 
 # Example without iFrame
 
@@ -39,24 +36,22 @@ my_alert.dismiss() # used to click on the 'Cancel' button of the alert
 my_frame = driver.find_element(By.ID, 'myFrame2')# here is WebElement
 driver.switch_to.frame(my_frame)
 driver.find_element(By.CSS_SELECTOR, '.last > li:nth-child(1) > a:nth-child(1)').click()
-time.sleep(2)
+
 
 # if we want to go back and click on the button outside the iFrame, we have to change the focus again
 driver.switch_to.default_content()
 driver.find_element(By.ID, 'btnOutFrame').click()
 print(driver.switch_to.alert.text)
-time.sleep(2)
 driver.switch_to.alert.dismiss()
 
 # We can specify the id directly
 driver.switch_to.frame('myFrame1')
 driver.find_element(By.CSS_SELECTOR, '.ddsmoothmenu > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)').click()
-time.sleep(2)
 print('I clicked in frame')
 
 
 # to find out how many frame's are on the page
-frames = driver.find_elements(By.TAG_NAME,"iframe")
+frames = driver.find_elements(By.TAG_NAME,"iFrame")
 
 for frame in frames:
     print(frame.get_attribute("id"))
