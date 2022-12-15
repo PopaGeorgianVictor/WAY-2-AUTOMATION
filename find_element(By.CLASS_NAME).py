14828be68d4634d0bd9f72c4825c75ca049ce5ba
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-15 15:43:33 +0200

    way 2 automation

--- find_element(By.CLASS_NAME).py
+++ find_element(By.CLASS_NAME).py
@@ -12,11 +12,10 @@ driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 driver.maximize_window()
 
 # URL of website
-url = "https://popageorgianvictor.github.io/MyTestSite/"
+url = "https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/all_in_one"
 
 # Opening the website
 driver.get(url)
 
-# The “p” element can be located like this:
-content = driver.find_element(By.CLASS_NAME, 'content')
-print(content)
+# element can be located like this:
+content = driver.find_element(By.CLASS_NAME, 'button')
