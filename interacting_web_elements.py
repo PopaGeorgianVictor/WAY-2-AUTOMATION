8dfc33049e654decd01f5594f90c4c0228afb98a
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-15 17:37:13 +0200

    way 2 automation

--- interacting_web_elements.py
+++ interacting_web_elements.py
@@ -1,4 +1,4 @@
-# importing webdriver from selenium
+
 
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -11,7 +11,7 @@ driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 
 # URL of website
-url = "https://popageorgianvictor.github.io/MyTestSite/"
+url = "https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/social_login_form"
 
 # Opening the website
 driver.get(url)
@@ -19,42 +19,30 @@ driver.get(url)
 # Interacting with web element
 
 # Maximize web window
-
 driver.maximize_window()
 
 # Performing click event
-
-click = driver.find_element(By.CSS_SELECTOR, '#loginForm > a:nth-child(8)')
-click.click()
+driver.find_element(By.CSS_SELECTOR, '.fb.btn').click()
 
 # Sending inputs
+driver.find_element(By.NAME, 'username').send_keys('username')
+driver.find_element(By.NAME, 'password').send_keys('password')
 
-username = driver.find_element(By.NAME, 'username')
-username.send_keys('username')
-
-password = driver.find_element(By.NAME, 'password')
-password.send_keys('password')
 
 # Clearing inputs
+driver.find_element(By.XPATH, "//input[@placeholder='Password']").clear()
+driver.find_element(By.XPATH, "//input[@placeholder='Username']").clear()
 
-username = driver.find_element(By.XPATH, '/html/body/form/input[1]')
-username.clear()
-
-password = driver.find_element(By.XPATH, '/html/body/form/input[2]')
-password.clear()
 
 # Navigating forward/back in browser history
-
-driver.find_element(By.LINK_TEXT, 'Continue').click()
+driver.find_element(By.CSS_SELECTOR, '.twitter.btn').click()
 driver.forward()
 driver.back()
 
 
 # Closing browser
-
 driver.close() # close current tab
 driver.quit() # close all tabs
 
 # Refresh/reload a web page
-
 driver.refresh()
