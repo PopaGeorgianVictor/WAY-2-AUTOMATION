a5b69ed299c93e7a6e20d64ad04e5edafe592192
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-20 14:55:38 +0200

    way 2 automation

---
+++ search.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/search_bar")
+driver.maximize_window()
+driver.implicitly_wait(2)
+
+
+# searck for ...
+search_bar = driver.find_element(By.ID, 'myInput')
+search_bar.send_keys('lists')
+
+# click on  .. after seacrh
+
+driver.find_element(By.XPATH,"//a[normalize-space()='LISTS']").click()
