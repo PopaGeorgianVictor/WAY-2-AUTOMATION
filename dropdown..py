de0407e03fc0ed71fcab4e8c7ffa52bb7a3769be
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 17:10:54 +0200

    way 2 automation

--- dropdown..py
+++ dropdown..py
@@ -19,17 +19,20 @@ driver.get(url)
 
 
 
-# using Select class
+# # using Select class
 my_dropdown = driver.find_element('id', 'coding-language-select')
 dropdown_object = Select(my_dropdown)
 dropdown_object.select_by_value("Python")
+dropdown_object.select_by_value('Java')
+dropdown_object.select_by_value('PHP')
+dropdown_object.select_by_value('C#')
+dropdown_object.select_by_value('SQL')
 all_options = dropdown_object.options
 for option in all_options:
     print(option.text)
-
-
-# using CSS
-dropdown_btn = driver.find_element('id', 'dropdownMenuButton')
-dropdown_btn.click()
-my_option = driver.find_element('xpath', '//*[@id="dropdowns"]/div[2]/div/ul/li[1]/a')
-my_option.click()
+#
+# # using CSS
+# dropdown_btn = driver.find_element('id', 'dropdownMenuButton')
+# dropdown_btn.click()
+# my_option = driver.find_element('xpath', '//*[@id="dropdowns"]/div[2]/div/ul/li[1]/a')
+# my_option.click()
