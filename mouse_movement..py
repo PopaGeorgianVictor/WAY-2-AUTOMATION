fc2a9c21fa8e8b808bee323fee3a69c5013efdd7
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 18:45:37 +0200

    way 2 automation

--- mouse_movement..py
+++ mouse_movement..py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
@@ -27,5 +28,16 @@ print('Mouse over success')
 
 # mouse hover and click
 print('Click on Portofolio')
-driver.find_element(By.XPATH, "//a[contains(text(),'Portofolio')]").click()
+# driver.find_element(By.XPATH, "//a[contains(text(),'Portofolio')]").click()
+link = driver.find_element(By.LINK_TEXT, "Portofolio")
+action.move_to_element(link).click().perform()
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[1])
+print("Second window title = " + driver.title)
 
+try:
+    driver.find_element(By.LINK_TEXT, "PORTOFOLIO")
+    print('Element exist')
+
+except NoSuchElementException:
+    print("Element does not exist")
