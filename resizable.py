c81b5e23363d298a22b7221859e836c03c814e29
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-27 10:18:53 +0200

    way 2  automation

--- resizable.py
+++ resizable.py
@@ -17,11 +17,16 @@ elem = driver.find_element(By.ID, 'resizable')
 print("Original size is", elem.size)
 resizable = driver.find_element(By.XPATH, '//*[@id="resizable"]/div[3]')
 ActionChains(driver).drag_and_drop_by_offset(resizable,500,500).perform()
+print("After resize is", elem.size)
 
-
+expected_size = {'height': 619, 'width': 619}
+actual_size = elem.size
+assert expected_size == actual_size, f"Error: expected: {expected_size}, actual: {actual_size}"
 
 # back to original
 
 ActionChains(driver).drag_and_drop_by_offset(resizable,-500,-500).perform()
-
+expected_size = {'height': 119, 'width': 119}
+actual_size = elem.size
+assert expected_size == actual_size, f"Error: expected: {expected_size}, actual: {actual_size}"
 
