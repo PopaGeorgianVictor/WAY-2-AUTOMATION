73ca5a3e919f3540f93becf14e865c56d7f3f9a0
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-10-26 15:47:20 +0300

    Update sliders.py

--- sliders.py
+++ sliders.py
@@ -11,7 +11,12 @@ driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/sliders")
 driver.maximize_window()
 driver.implicitly_wait(2)
 
+
 slider = driver.find_element(By.ID,"myRange")
 size = slider.size
 w = size['width']
 ActionChains(driver).drag_and_drop_by_offset(slider,w/2,0).perform()
+
+# location of sliders 
+
+print(slider.location)
